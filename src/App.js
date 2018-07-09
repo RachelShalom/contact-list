5c8a80a7ff885f11c9d37bca7a56b83f02ce68aa
RachelWork <ms.rachel.shalom@gmail.com>
2018-07-09 19:08:53 +0300

    add state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-
+import ListContacts from './listContacts';
 class App extends Component {
+  state={
+    contacts:[
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+     <ListContacts contacts={this.state.contacts}/>
     )
   }
 }
