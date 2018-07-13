f9bf1e44137c1a19a6744364556ed7cc87667f1b
Rachel <rshalom1984@gmail.com>
2018-07-13 19:54:54 +0300

    add from for adding new contact

--- src/App.js
+++ src/App.js
@@ -24,13 +24,22 @@ class App extends Component {
         ContactsAPI.remove(contact);
     }
 
-
+    createContact(contact) {
+            ContactsAPI.create(contact).then(contact => {
+              this.setState(state => ({
+                contacts: state.contacts.concat([ contact ])
+              }))
+            })
+          }
 
     render() {
       
         return ( <div>
             <Route exact path='/' render={()=>(<ListContacts onDeleteContact = { this.removeContact } onNavigate={()=>{this.setState({screen:'create'})}} contacts = { this.state.contacts } /> )}/>  
-            <Route path='/create' component={CreateContacts}/>
+            <Route path='/create'  render={({history})=>(<CreateContacts 
+            onCreateContacts={((contact)=>{this.createContact(contact)
+            history.push('/');
+            })}/>)}/>
                  </div>)
         
     }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,40 @@
 import React,{Component} from 'react'
+import {Link} from 'react-router-dom'
+import ImageInput from './ImageInput';
+import formSerialize from 'form-serialize'
 
 
 
 class CreateContacts extends Component{
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        const values= formSerialize(e.target,{hash:true});//the hash:true turn the info from the form to an object
+        console.log(values);
+        if(this.props.onCreateContacts){
+            this.props.onCreateContacts(values);//this is to send the data to App.js
+        }
+    }
     render(){
-        return( <div>hello world</div>);
+        return( <div>
+            <Link to='/' className='close-create-contact'></Link>
+            <form onSubmit={this.handleSubmit} className='create-contact-form'>
+            <ImageInput 
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+            />
+            <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='NAME'/>
+            <input type='text' name='email' placeholder='EMAIL'/>
+            <button>Add Contact</button>
+            </div>
+        
+            </form>
+
+
+
+
+        </div>);
        
     }
 }
